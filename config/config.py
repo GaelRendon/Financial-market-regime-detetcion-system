f7a57cf9bf33b752714a5623f1f1015e7329e148
GaelRendon <grenmen@outlook.com>
2026-06-26 14:18:41 -0600

    Add candidate elbow and optimal cluster configuration parameters

--- config/config.py
+++ config/config.py
@@ -45,6 +45,10 @@ CLUSTERING_ALGORITHM = "kmeans"
 MIN_CLUSTERS = 2
 MAX_CLUSTERS = 10
 
+# Candidate elbow identified during the evaluation
+ELBOW_K = None
+
+# Optimal number of clusters
 OPTIMAL_N_CLUSTERS = None
 
 # This is used to ensure consistency of the results
